ca9d1d0f8aa143507a15035d98fcf5f3dfca3c11
Gan-erdene-just-do-it <Lkhagvasurenerdenesugar@gmail.com>
2024-08-24 13:13:58 +0800

    gg

---
+++ yu.js
@@ -0,0 +1,61 @@
+/// if else -> nuhtsul shalgakh 
+//tenstuu bnu ih bain u baga bnu
+var age=15
+//> ,< ,>,< == !=
+if (age>=18) {
+    console.log("Ta nasand hursen")
+    }       
+    else{
+        console.log("ta nasand hursen")
+    }
+    var tool=30 =10 ;
+    if ("too>too") {
+        console.log (too1+" ih baina"+too2+"baga baina");
+    }
+var too=8 //8%3=2 3%1=0// // tentsuu bish
+ console.log("sorog too")
+ console.log("eyreg too")
+ // && ->and,bas ||-> or,esvel
+ var nas=25
+ var huis="er em medegdehgui"
+ if(nas>17 && huis=="meedgui") {
+    console.log ("nasand hursen")
+ }else if (nas<18 && huis== "emegtei");{
+   }  console.log ("nasand hureegui em");{
+   } if ("ta nasasnd hureegui eregtei") ;
+    var  numl=25  =8 =3
+    if ("num1+num2+num3");
+    var dun=+prompt("dungee oruulanuu")
+    if (dun>=0 && dun<=59){
+        ;
+    }
+    alert ("taniig aavchine unt gev");
+    alert("ta utas uh uu esvel untah uu" );
+    var choice = prompt ("uhah/untah");
+    if  (choice=="uhah"); 
+    alert("Ta uuriin 3 hutel utas uhav");
+    alert("Ta sarnii tuyand ortson bna");
+    alert("ta zuudendee serev ");
+    alert("TA HAYGDSAN gudmanjid sersen bna")
+    alert("ene hotiin humuus nuusen")
+    var choice = prompt ("zuun/baruun")
+    if (choice=="baruun");
+    alert("ta gazaraas alakh olov");
+    alert("ta baavgaitai taarav");
+    var choice = prompt ("alalzakh/zugtaah");
+    if (choice=="zugtaah");
+    alert("Ta 50kl meteriin tsaan hot olov");
+    alert("ta tend ochij shine amdril ehluulee")
+    alert("ta zuudneesee serj hojov");
+
+    
+   
+    
+    
+  
+        
+
+     
+        
+
+
